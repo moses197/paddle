ee4a89d26dff03a8cfa2da203144196acba5606a
moses197 <leomoszeko@gmail.com>
2023-04-19 20:59:15 +0100

    Sign up and some other configuration

--- paddle/item/models.py
+++ paddle/item/models.py
@@ -14,7 +14,7 @@ class Category(models.Model):
     
 
 class Item(models.Model):
-    Category = models.ForeignKey(Category, related_name='items', on_delete=models.CASCADE)
+    category = models.ForeignKey(Category, related_name='items', on_delete=models.CASCADE)
     name = models.CharField(max_length=255)
     description = models.TextField(blank=True, null=True)
     price = models.FloatField()
